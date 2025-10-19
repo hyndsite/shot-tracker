b2b5358efe57f4d78d586a436c602f7c652e3fcf
Max McCarty <max.r.mccarty@gmail.com>
2025-10-19 18:15:29 -0400

    Testing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.75j03efggig"
+    "revision": "0.7shqli0102"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
